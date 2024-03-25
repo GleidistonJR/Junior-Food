62c122d8cd18ae1954cb2dffcc228c561015d535
GleidistonJR <gleidistonjunior@gmail.com>
2024-03-25 10:23:16 -0300

    armazenando array em json...

--- compilador-pedido.php
+++ compilador-pedido.php
@@ -1,17 +1,20 @@
 <?php
-    $lancheJson = json_decode(file_get_contents('data/lanches/hamburgueres/hamburguer-gemeos.json')); 
-
-    //print_r($lancheJson);
-
-    foreach ($lancheJson as $dado){
-        print_r($dado);
-    }
-
-    $scriptTexto = '';
-?>
+    $carrinho = json_decode(file_get_contents('data/lanches/hamburgueres/hamburguer-gemeos.json'), true);
 
 
+echo '<pre>';
+    $arrayPedido = array();
+    //Existe duas formas de adiconar elemento ao array
+    //Primeira por funçao
+    array_push($arrayPedido, $carrinho['titulo']);
+    //Segunda por deixar o indice vazio, sera adiconada na ultima posiçao
+    $arrayPedido[] = $carrinho['preco']; 
 
 
 
-https://www.youtube.com/watch?v=MCJRmZSmIQk
+    //Guardando array em json
+$arquivoJson = fopen('pedido.json', 'w');
+fwrite($arquivoJson, json_encode($arrayPedido));
+fclose($arquivoJson);
+
+print_r($carrinho);
