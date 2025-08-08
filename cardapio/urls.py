c688fa6652ee7eb39405f3b655723f3ba0c04623
GleidistonJR <gleidistonjunior@gmail.com>
2025-08-08 06:23:21 -0300

    iniciando django rest

--- cardapio/urls.py
+++ cardapio/urls.py
@@ -1,8 +1,16 @@
 from cardapio import views
-from django.urls import path
+from django.urls import path, include
+
+from rest_framework.routers import DefaultRouter
+from cardapio.views import ProdutoViewSet
+
+router = DefaultRouter()
+router.register(r'produtos', ProdutoViewSet)
+
 
 app_name = 'cardapio'
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('api/', include(router.urls)),
 ]
--- cardapio/views.py
+++ cardapio/views.py
@@ -1,7 +1,14 @@
+from rest_framework.viewsets import ModelViewSet
+from .models import Produto
+from .serializers import ProdutoSerializer
+
 from django.shortcuts import render, get_object_or_404
 #from cardapio.models import Cardapio
 
-# Create your views here.
+class ProdutoViewSet(ModelViewSet):
+    queryset = Produto.objects.all()
+    serializer_class = ProdutoSerializer
+
 
 def index(request):
 
